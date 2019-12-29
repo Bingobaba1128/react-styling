ab63e82e541ab4304e054f0cc1340c411e85a288
Joey.Shen <joey.shen1128@gmail.com>
2019-12-29 14:03:50 +1300

    Adding comment in App.js

--- src/App.js
+++ src/App.js
@@ -83,6 +83,7 @@ class App extends Component {
           {persons}
         </div>
     );
+    //s
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
